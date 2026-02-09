ed19813e8e2496e845f8efbc5a059bfb383b31c9
Ove Ragnar Oddløkken <oroddlokken@users.noreply.github.com>
2026-02-09 20:28:11 +0100

    Update sha256 checksum for dogcat formula

--- Formula/dogcat.rb
+++ Formula/dogcat.rb
@@ -2,7 +2,7 @@ class Dogcat < Formula
   desc "lightweight, file-based issue tracking and memory upgrade for AI agents (and humans!)"
   homepage "https://github.com/oroddlokken/dogcat"
   url "https://github.com/oroddlokken/dogcat/releases/download/v0.5.6/dogcat-0.5.6-py3-none-any.whl"
-  sha256 "970d94d3420162489a5b8ba94b4507737e6565f52a13c4774ba0e0f7ecbb99d9"
+  sha256 "8a8dda3d72cf3d17dd75f006d4828832e69cb61f9a9cb967ac13c38d80571d96"
   license "MIT"
 
   depends_on "uv"
