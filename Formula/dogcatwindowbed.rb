0d12b671924b33aafe705f07acd1f4b552002214
Ove Ragnar Oddløkken <post@rykroken.net>
2026-02-10 20:01:56 +0100

    Test

--- Formula/dogcatwindowbed.rb
+++ Formula/dogcatwindowbed.rb
@@ -1,26 +1,21 @@
-class Dogcat < Formula
+class Dogcatwindowbed < Formula
+  include Language::Python::Virtualenv
+
   desc "lightweight, file-based issue tracking and memory upgrade for AI agents (and humans!)"
   homepage "https://github.com/oroddlokken/dogcat"
   url "https://github.com/oroddlokken/dogcat/releases/download/v0.5.8/dogcat-0.5.8-py3-none-any.whl"
   sha256 "f0012e324d87213f68c55d4efabfe2171a604e0b19287f954832d5c07f2becd2"
   license "MIT"
 
-  depends_on "uv"
+  depends_on "python@3.14"
 
   def install
-    libexec.install Dir["*"]
-    (bin/"dcat").write <<~BASH
-      #!/bin/bash
-      exec "#{libexec}/.venv/bin/dcat" "$@"
-    BASH
+    venv = virtualenv_create(libexec, "python3.14")
+    wheel = buildpath/"dogcat-#{version}-py3-none-any.whl"
+    cp cached_download, wheel
+    venv.pip_install wheel
+    bin.install_symlink libexec/"bin/dcat"
     bin.install_symlink "dcat" => "dogcat"
-  end
-
-  def post_install
-    ENV["UV_CACHE_DIR"] = HOMEBREW_CACHE/"uv"
-    wheel = Dir[libexec/"dogcat-*.whl"].first
-    system "uv", "venv", "--python", "3.13", libexec/".venv"
-    system "uv", "pip", "install", "--python", libexec/".venv/bin/python", wheel
   end
 
   test do
