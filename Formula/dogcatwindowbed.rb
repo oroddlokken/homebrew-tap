d5904aa62dd70d57fa17047552664277e042cece
Ove Ragnar Oddløkken <post@rykroken.net>
2026-02-19 22:11:17 +0100

    Update Formula/dogcatwindowbed.rb

--- Formula/dogcatwindowbed.rb
+++ Formula/dogcatwindowbed.rb
@@ -3,8 +3,8 @@ class Dogcatwindowbed < Formula
 
   desc "lightweight, file-based issue tracking and memory upgrade for AI agents (and humans!)"
   homepage "https://github.com/oroddlokken/dogcat"
-  url "https://github.com/oroddlokken/dogcat/releases/download/v0.10.0-rc.1/dogcat-0.10.0-rc.1-py3-none-any.whl"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/oroddlokken/dogcat/releases/download/v0.10.0/dogcat-0.10.0-py3-none-any.whl"
+  sha256 "a96a596309faec676628d6c65c0a41eb12abb0dabaecac490218f2291bcba217"
   license "MIT"
 
   depends_on "python@3.14"
